2bfc69192f44eaae00b633e0953f43d105b90dee
Brian W <antiantigoose@gmail.com>
2025-05-02 00:03:47 -0700

    tilemap tiles update

---
+++ HelloWorldQuestionMark/Assets/Scripts/LoadScene1.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadScene1 : MonoBehaviour
+{
+    void Start()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
