6aebb7dd4541bec784159b503007613424be4187
bkallencf <benjaminryokallen@gmail.com>
2025-10-30 18:49:16 -0700

    Enemy class

--- HelloWorldQuestionMark/Assets/Scripts/Enemy.cs
+++ HelloWorldQuestionMark/Assets/Scripts/Enemy.cs
@@ -1,4 +1,23 @@
-public interface Enemy
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
 {
-    
+    [Header("Enemy")]
+    [SerializeField] private Transform enemy;
+    [SerializeField] public int health = 1;
+    [SerializeField] public int damage = 1;
+    [SerializeField] public float enemySpeed;
+
+    private EnemyHealth enemyHealth;
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        // Creates a new EnemyHealth
+        enemyHealth = enemy.GetComponent<EnemyHealth>();
+        enemyHealth.Init(health, damage);
+
+        spriteRenderer = enemy.GetComponent<SpriteRenderer>();
+    }
+
 }
